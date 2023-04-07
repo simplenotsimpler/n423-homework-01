142d156e853a6ceaeb348f936965d959750a96bb
SimpleNotSimpler <simplenotsimpler@outlook.com>
2023-04-07 14:46:34 -0400

    feature: start about page

---
+++ src/pages/about.js
@@ -0,0 +1,21 @@
+const About = () => {
+  return (
+    <section>
+      <article className="card">
+        <img src="../public/images/owner.jpg" className="round-img" alt="" />
+        <h1 className="card-title">The Owner:</h1>
+        <p className="card-body">
+          Lorem ipsum dolor sit amet, consectetur adipiscing elit. Vestibulum ac
+          bibendum quam, vel finibus leo. Morbi mattis, odio sed fermentum
+          convallis, dolor lorem porta magna, eu consequat lorem magna a turpis.
+          Vivamus ultrices elit risus, at egestas enim tempus non. Vivamus
+          molestie lacinia dui in tincidunt. Morbi vulputate accumsan
+          scelerisque. Nunc justo turpis, blandit ac vestibulum congue,
+          ullamcorper mattis orci.
+        </p>
+      </article>
+    </section>
+  );
+};
+
+export default About;
