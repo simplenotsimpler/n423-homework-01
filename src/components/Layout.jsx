081a93ed61cebfdc6c33b70d7d6bc42cd6bb9f02
SimpleNotSimpler <simplenotsimpler@outlook.com>
2023-04-07 14:24:13 -0400

    feature: add hero

--- src/components/Layout.jsx
+++ src/components/Layout.jsx
@@ -1,10 +1,12 @@
 import Footer from "./Footer.jsx";
+import Hero from "./Hero.jsx";
 import Navbar from "./Navbar.jsx";
 
 const Layout = ({ children }) => {
   return (
     <div className="container">
       <Navbar />
+      <Hero />
       <main>{children}</main>
       <Footer />
     </div>
