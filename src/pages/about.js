e73c0173d8dc9bdfefa30d493089ee3ad6664173
SimpleNotSimpler <simplenotsimpler@outlook.com>
2023-04-07 16:16:24 -0400

    fix: more about styling, still image load issue

--- src/pages/about.js
+++ src/pages/about.js
@@ -1,18 +1,27 @@
+import styles from "../styles/About.module.css";
+import CardStyles from "../styles/Card.module.css";
+
 const About = () => {
   return (
-    <section>
-      <article className="card">
-        <img src="../public/images/owner.jpg" className="round-img" alt="" />
-        <h1 className="card-title">The Owner:</h1>
-        <p className="card-body">
-          Lorem ipsum dolor sit amet, consectetur adipiscing elit. Vestibulum ac
-          bibendum quam, vel finibus leo. Morbi mattis, odio sed fermentum
-          convallis, dolor lorem porta magna, eu consequat lorem magna a turpis.
-          Vivamus ultrices elit risus, at egestas enim tempus non. Vivamus
-          molestie lacinia dui in tincidunt. Morbi vulputate accumsan
-          scelerisque. Nunc justo turpis, blandit ac vestibulum congue,
-          ullamcorper mattis orci.
-        </p>
+    <section className={styles.about}>
+      <article className={CardStyles.card}>
+        <img
+          src="/images/owner.jpg"
+          className={CardStyles.roundImg}
+          alt="Owner"
+        />
+        <div className={CardStyles.cardBody}>
+          <h1 className={CardStyles.cardTitle}>The Owner:</h1>
+          <p className={CardStyles.cardText}>
+            Lorem ipsum dolor sit amet, consectetur adipiscing elit. Vestibulum
+            ac bibendum quam, vel finibus leo. Morbi mattis, odio sed fermentum
+            convallis, dolor lorem porta magna, eu consequat lorem magna a
+            turpis. Vivamus ultrices elit risus, at egestas enim tempus non.
+            Vivamus molestie lacinia dui in tincidunt. Morbi vulputate accumsan
+            scelerisque. Nunc justo turpis, blandit ac vestibulum congue,
+            ullamcorper mattis orci.
+          </p>
+        </div>
       </article>
     </section>
   );
